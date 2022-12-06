777008ec7c25c20f4b232430c0b21dcf5da218e4
bcompton1 <115109936+bcompton1@users.noreply.github.com>
2022-12-06 14:23:00 -0800

    Create Challenge-1-booklist.sql

---
+++ Challenge-1-booklist.sql
@@ -0,0 +1,8 @@
+/**books**/
+
+CREATE TABLE books (id INTEGER PRIMARY KEY, name TEXT, rating INTEGER);
+
+INSERT INTO books VALUES (1, "Little Women", 1);
+INSERT INTO books VALUES (2, "War and Peace", 2);
+INSERT INTO books VALUES (3, "FOG", 3);
+SELECT * FROM books;
